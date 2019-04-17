936fca66ad7bf6bce8e203e30df66e06139b0b2f
jstray <jonathanstray@gmail.com>
2019-04-16 21:49:26 -0700

    Initial version

---
+++ test_xpathextractor.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python3
+import unittest
+import warnings
+import pandas as pd
+from pandas.testing import assert_frame_equal
+from xpathextractor import parse_document, select, xpath, render
+
+class UnittestRunnerThatDoesntAddWarningFilter(unittest.TextTestRunner):
+    def __init(self, *args, **kwargs):
+        print(repr((args, kwargs)))
+        super().__init__(*args, **kwargs, warnings=None)
+
+
+class Xml1(unittest.TestCase):
+    def setUp(self):
+        self.tree = parse_document(
+            (
+                '<a><b><c>c</c><d foo="bar">d</d></b><b><c>C</c>'
+                '<d foo="baz">D</d></b><e>ehead<f>f</f>etail</e></a>'
+            ),
+            False
+        )
+
+    def select(self, selector):
+        return select(self.tree, xpath(selector))
+
+    def test_convert_node_to_text(self):
+        self.assertEqual(self.select('//c'), ['c', 'C'])
+
+    def test_convert_subnodes_to_text(self):
+        self.assertEqual(self.select('//b'), ['cd', 'CD'])
+
+    def test_attributes(self):
+        self.assertEqual(self.select('//d/@foo'), ['bar', 'baz'])
+
+    def test_text(self):
+        self.assertEqual(self.select('//d/text()'), ['d', 'D'])
+
+    def test_head(self):
+        self.assertEqual(self.select('//f/preceding-sibling::text()'),
+                         ['ehead'])
+
+    def test_tail(self):
+        self.assertEqual(self.select('//f/following-sibling::text()'),
+                         ['etail'])
+
+    def test_count(self):
+        self.assertEqual(self.select('count(//d)'), [2.0])
+
+    def test_bool(self):
+        self.assertEqual(self.select('boolean(//f)'), [True])
+        self.assertEqual(self.select('boolean(//g)'), [False])
+
+
+class Html1(unittest.TestCase):
+    def setUp(self):
+        self.tree = parse_document(
+            '''<!DOCTYPE html><html>
+              <head>
+                <meta charset="utf-16be">
+                <title>Hello, world!</title>
+                <link rel="stylesheet" href="/style.css"/>
+                <script src="/script.js"></script>
+              </head>
+              <body>
+                <img src="/logo.png" alt="logo" />
+                <p>Foo</p>
+                <p>Bar</p>
+                <table><td>Single-cell table</table>
+                <a href="/foo">Foo</a>
+                <a href="/bar">Bar</a>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2 2">
+                  <path d="M0 0L2 2"/>
+                </svg>
+              </body>
+            </html>''',
+            True
+        )
+
+    def select(self, selector):
+        return select(self.tree, xpath(selector))
+
+    def test_simple(self):
+        self.assertEqual(self.select('//p'), ['Foo', 'Bar'])
+
+    def test_do_not_expand_single_string(self):
+        self.assertEqual(self.select("'ab'"), ['ab'])
+
+    def test_svg_namespace(self):
+        # Works across namespaces
+        self.assertEqual(self.select('//svg:path/@d'), ['M0 0L2 2'])
+
+    def test_add_missing_elements(self):
+        # Parse invalid HTML by adding missing elements
+        self.assertEqual(self.select('//tr'), ['Single-cell table'])
+
+
+# class HtmlTest(unittest.TestCase):
+#     def test_no_warning_coercing_non_xml_name(self):
+#         # Turn warning into error (just for this test -- the test runner resets
+#         # filters each test)
+#         warnings.simplefilter('error', append=True)
+#         parse_document('<ns:html></ns:html>', True)
+
+
+class XpathExtractorTest(unittest.TestCase):
+    def setUp(self):
+        doc1 = '''    
+        <!DOCTYPE html><html>
+          <head>
+            <meta charset="utf-16be">
+            <title>Scrape me please</title>
+            <link rel="stylesheet" href="/style.css"/>
+            <script src="/script.js"></script>
+          </head>
+          <body>
+            <h2>This is outside the l1</h2>
+            <img src="/logo.png" alt="logo" />
+            <ul>
+                <li>
+                    <h1>A title</h1>
+                    <p>A description</p>
+                </li>
+                <li>
+                    <h1>B title</h1>
+                    <p>B description</p>
+                </li>
+            </ul>
+          </body>'''
+
+        doc2 = '''     
+        <!DOCTYPE html><html>
+          <head>
+            <meta charset="utf-16be">
+            <title>Link scraping test</title>
+            <link rel="stylesheet" href="/style.css"/>
+            <script src="/script.js"></script>
+          </head>
+          <body>
+            <img src="/logo.png" alt="logo" />
+            <ul>
+                <li>
+                    <h1>C title</h1>
+                    <p>C description</p>
+                    <p>D description</p>
+                </li>
+            </ul>   
+            <table>
+                <tr>
+                    <th>Name<th>
+                    <th>link<th>
+                </tr>
+                <tr>
+                    <td>Orange<td>
+                    <td><a href='http://orange.com'>Orange link</a></td>
+                </tr>
+                <tr>
+                    <td>Red<td>
+                    <td><a>Red no href</a></td>
+                </tr>
+                <tr>
+                    <td>Blue<td>
+                    <td><a href='http://blue.com'>Blue link</a></td>
+                </tr>
+            </table>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2 2">
+              <path d="M0 0L2 2"/>
+            </svg>
+          </body>'''
+
+        self.table = pd.DataFrame({'html':[doc1, doc2]})
+
+
+    def test_extract_links(self):
+        # This is the default parameter setting. 
+        # - Should pick up rows only from doc2, as doc1 has no <a>
+        # - Should work even though second link has no URL
+        params = { 
+            'rowxpath':'//a', 
+            'colselectors' : [
+                {'colxpath':'.', 'colname':'Text'},
+                {'colxpath':'./@href', 'colname':'URL'},
+            ]}
+        out = render(params, self.table)
+        expected = pd.DataFrame({
+                'Text':['Orange link','Red no href', 'Blue link'],
+                'URL':['http://orange.com','', 'http://blue.com']
+            })
+        assert_frame_equal(out, expected)
+
+    def test_multiple_colvals_multiple_pages(self):
+        # Find elements within multiple pages.
+        # In this test data, some records have multiply defined column values, 
+        # which should be concatenated with spaces between
+        params = { 
+            'rowxpath':'//li', 
+            'colselectors' : [
+                {'colxpath':'h1', 'colname':'Title'},
+                {'colxpath':'p', 'colname':'Description'},
+            ]}
+        expected = pd.DataFrame({
+                'Title':['A title', 'B title', 'C title'],
+                'Description':['A description','B description', 'C description D description']
+            })
+
+        out = render(params, self.table)
+        assert_frame_equal(out, expected)
+
+    def test_no_rowxpath(self):
+        # Use the "zip" extraction algorithm. Should pad all columns to same length 
+        # if needed, and give a warning if we did.
+        params = { 
+            'rowxpath':'', 
+            'colselectors' : [
+                {'colxpath':'//h1', 'colname':'Title'},
+                {'colxpath':'//p', 'colname':'Description'},
+            ]}
+        expected = pd.DataFrame({
+                'Title':['A title', 'B title', 'C title', None],
+                'Description':['A description','B description', 'C description', 'D description']
+            })
+
+        out = render(params, self.table)
+        self.assertTrue(isinstance(out, tuple)) 
+        self.assertTrue(isinstance(out[1], str)) # warning message
+        assert_frame_equal(out[0], expected)
+
+    def test_empty_colselector(self):
+        # missing xpath should error
+        params = { 
+            'rowxpath':'//li', 
+            'colselectors' : [
+                {'colxpath':'', 'colname':'Title'},
+                {'colxpath':'p', 'colname':'Description'},
+            ]}
+        out = render(params, self.table)
+
+        self.assertTrue(isinstance(out, str)) # error message
+
+    def test_empty_colname(self):
+        # missing column name should error
+        params = { 
+            'rowxpath':'//li', 
+            'colselectors' : [
+                {'colxpath':'.', 'colname':'Title'},
+                {'colxpath':'p', 'colname':''},
+            ]}
+        out = render(params, self.table)
+
+        self.assertTrue(isinstance(out,str)) # error message
+
+if __name__ == '__main__':
+    unittest.main(testRunner=UnittestRunnerThatDoesntAddWarningFilter())
---
+++ xpathextractor.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+
+from typing import Callable, List, Tuple
+import warnings
+from html5lib.constants import DataLossWarning
+from lxml import etree
+from lxml.html import html5parser
+import pandas as pd
+
+# GLOBALLY ignore the warnings that (hopefully) only this module will emit. The
+# warnings all have to do with "invalid" HTML, but that HTML is often good
+# enough for our users so it isn't worth dumping anything to stderr.
+warnings.filterwarnings('ignore', category=DataLossWarning,
+                        module=r'html5lib\._ihatexml')
+
+def xpath(s: str) -> etree.XPath:
+    """
+    Parse an XPath selector, or raise etree.XPathSyntaxError.
+
+    A word on namespaces: this module parses HTML without a namespace.
+    It parses embedded SVGs in the "svg" namespace. So your XPath
+    selectors can look like:
+
+    xpath('//p')           # all <p> tags (in HTML)
+    xpath('//order/@id')   # all <order> id attributes (in XML)
+    xpath('//svg:path/@d') # all <path> tags (in SVG embedded within HTML)
+    """
+    return etree.XPath(
+        s,
+        smart_strings=True,  # so result strings don't ref XML doc
+        namespaces={
+            'svg': 'http://www.w3.org/2000/svg',
+        }
+    )
+
+
+def parse_document(text: str, is_html: bool) -> etree._Element:
+    """Build a etree root node from `text`.
+
+    Throws TODO what errors?
+    """
+    if is_html:
+        parser = html5parser.HTMLParser(namespaceHTMLElements=False)
+        tree = html5parser.fromstring(text, parser=parser)
+        return tree
+    else:
+        parser = etree.XMLParser(
+            encoding='utf-8',
+            # Disable as much as we can, for security
+            load_dtd=False,
+            collect_ids=False,
+            resolve_entities=False
+        )
+        return etree.fromstring(text.encode('utf-8'), parser)
+
+
+def _item_to_string(item) -> str:
+    """Convert an XPath-returned item to a string.
+
+    Rules:
+    text node => text contents
+    """
+    if hasattr(item, 'itertext'):
+        # This is an Element.
+        return ''.join(item.itertext())
+    else:
+        # item.is_attribute
+        # item.is_text
+        # item.is_tail
+        return str(item)
+
+
+def select(tree: etree._Element, selector: etree.XPath) -> List[str]:
+    """
+    Run an xpath expression on `tree` and convert results to strings.
+
+    Raise XPathEvalError on error.
+    """
+    # TODO avoid DoS. xpath selectors can take enormous amounts of CPU/memory
+    result = selector(tree)
+    if hasattr(result, '__iter__') and not isinstance(result, str):
+        return list(_item_to_string(item) for item in result)
+    else:
+        # count(//a) => float. Return list of float.
+        return [result]
+
+
+# Extract as many rows as there are elements mathing the record selector
+def add_rows_by_record(tree, rowxpath_string, colselectors, outtable):
+    rowxpath = xpath(rowxpath_string)
+    row_els = rowxpath(tree)
+
+    for el in row_els:
+        newrow = {}       
+        for col in colselectors:
+            colvals = select(el, xpath(col['colxpath']))
+
+            # If we have more than one element matching the column selector,
+            # just concatenate with spaces. If we have zero, empty column.
+            newrow[col['colname']] = (' ').join(colvals)
+
+        outtable = outtable.append(newrow, ignore_index=True)
+
+    return outtable
+
+# Extract columns separately, then zip them together. This is a weaker, potentially
+# more error-prone method, but also simpler for the user to set up
+def add_rows_by_zip(tree, colselectors, outtable):
+    column_lists = {}
+    maxlen = 0
+    for col in colselectors:
+        colvals = select(tree, xpath(col['colxpath']))
+        maxlen = max(maxlen, len(colvals))
+        column_lists[col['colname']] = colvals
+
+    # Pad all column lists to the same length with None
+    # If they're not all the same length, this may mean extraction failed. 
+    # Let the user see the data, and give them a warning
+    warn_user = False
+    for colname,colvals in column_lists.items():
+        padding = (maxlen-len(colvals))
+        if padding>0:
+            warn_user = True
+            column_lists[colname] += [None] * padding
+
+    newrows = pd.DataFrame(column_lists, columns=outtable.columns)
+    outtable = pd.concat([outtable, newrows], axis=0).reset_index(drop=True)
+    return (outtable, warn_user)
+
+
+def render(params, table):
+
+    inputcol = 'html'    # hardcode to enable Quick Fix suggestion to add Scrape HTML
+
+    rowxpath = params['rowxpath']
+
+    colselectors = params['colselectors']
+    outcolnames = [c['colname'] for c in colselectors]
+    if '' in outcolnames:
+        return 'Missing column name'
+    outcolpaths = [c['colxpath'] for c in colselectors]
+    if '' in outcolpaths:
+        return 'Missing column selector'
+
+    outtable = pd.DataFrame(columns=outcolnames)
+
+    # Loop over rows of input html column, each of which is a complete html document
+    # Concatenate rows extracted from each document.
+    first_different_length_row = None
+    for index,row in table.iterrows():
+        html_text = row[inputcol]
+
+        tree = parse_document(html_text, True) # is_html=true 
+
+        try:
+            if rowxpath != '':
+                outtable = add_rows_by_record(tree, rowxpath, colselectors, outtable)
+            else:
+                outtable,warn = add_rows_by_zip(tree, colselectors, outtable)
+
+                # track the first row where the extracted columns are not all the same length
+                if warn and not first_different_length_row: 
+                    first_different_length_row = index
+
+        except etree.XPathEvalError as err:
+            return (None, 'XPath error: %s' % err)
+
+    if first_different_length_row:
+        return (outtable, 
+                'Extracted columns of differing lengths from HTML on row %d' % first_different_length_row)
+    else:
+        return outtable
+
